109d7198e83c22f5699d0c251fc71e62cd61c0de
Shawn Rutledge <s@ecloud.org>
2018-07-03 08:55:57 +0200

    can insert and remove SD card while running; other improvements
    - reads sensors every 5 seconds and outputs readings to serial
    - writes the SD card within 5 seconds after insertion, and every
      60 seconds thereafter

--- adalogger-bme280.ino
+++ adalogger-bme280.ino
@@ -8,16 +8,19 @@
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
-const int chipSelect = 4;         // SPI chip select for SD card
-const int cardDetect = 7;          // pin that detects whether the card is there
-const int writeLed = 8;           // LED indicator for writing to card
-const int errorLed = 13;          // LED indicator for error
-const long interval = 9968;            // time between readings
-const char fileName[] = "datalog.csv"; // filename to save on SD card
-const char dataFieldNames[] = "Time,Battery(V),Temperature(*C),Humidity(%),Pressure(hPa)";
+const int chipSelect = 4;               // SPI chip select for SD card
+const int cardDetect = 7;               // pin that detects whether the card is there
+const int writeLed = 8;                 // LED indicator for writing to card
+const int errorLed = 13;                // LED indicator for error
+const long readInterval = 5000;         // time between readings
+const long logInterval = 60000;         // time between writings
+const char fileName[] = "datalog.csv";  // filename to save on SD card
+const char dataFieldNames[] = "Time,Battery(V),Temperature(°C),Humidity(%),Pressure(hPa)";
 
 Adafruit_BME280 bme; // I2C
-long lastWriteTime = 0;           // timestamp for last write attempt
+long lastReadTime = 0;                  // timestamp for last sensor readings
+long lastWriteTime = 0;                 // timestamp for last write to log file
+bool sdInited = false;
 
 float batteryVoltage() {
   float vbat = analogRead(VBATPIN);
@@ -27,25 +30,28 @@ float batteryVoltage() {
   return vbat;
 }
 
-boolean startSDCard() {
-  // Wait until the card is inserted:
-  while (digitalRead(cardDetect) == LOW) {
-    Serial.println("Waiting for card...");
+void blinkError(int times) {
+  for (int i = 0; i < times; ++i) {
     digitalWrite(errorLed, HIGH);
     delay(100);
     digitalWrite(errorLed, LOW);
     delay(100);
-    digitalWrite(errorLed, HIGH);
-    delay(100);
-    digitalWrite(errorLed, LOW);
-    delay(700);
+  }
+}
+
+bool startSDCard() {
+  // card is inserted?
+  if (digitalRead(cardDetect) == LOW) {
+    Serial.println("SD card not detected");
+    blinkError(2);
+    return false;
   }
 
-  // wait until the card initialized successfully:
-  while (!SD.begin(chipSelect)) {
-    digitalWrite(errorLed, HIGH);   // turn on error LED
-    Serial.println("Card failed");
-    delay(750);
+  // card is initialized successfully?
+  if (!SD.begin(chipSelect)) {
+    Serial.println("SD card failed");
+    blinkError(3);
+    return false;
   }
   return true;
 }
@@ -55,30 +61,16 @@ void setup() {
   pinMode(errorLed, OUTPUT);
   pinMode(cardDetect, INPUT_PULLUP);
 
+  digitalWrite(errorLed, LOW);
+  digitalWrite(writeLed, LOW);
+
   if (!bme.begin(0x76)) {
       Serial.println("Could not find a valid BME280 sensor, check wiring!");
       while (1) {
-          digitalWrite(errorLed, HIGH);
-          delay(100);
-          digitalWrite(errorLed, LOW);
-          delay(100);
+          blinkError(4);
+          delay(1000);
       }
   }
-
-  // startSDCard() blocks everything until the card is present and writable:
-  if (startSDCard() == true) {
-    Serial.println("card initialized.");
-    delay(100);
-    // open the log file:
-    File logFile = SD.open(fileName, FILE_WRITE);
-    // write header columns to file:
-    if (logFile) {
-      logFile.println(dataFieldNames);
-      logFile.close();
-    }
-  }
-
-  Serial.println(dataFieldNames);
 }
 
 int appendCommaFloat(char *s, int len, int offset, float v) {
@@ -86,37 +78,52 @@ int appendCommaFloat(char *s, int len, int offset, float v) {
 }
 
 void loop() {
-  // if the SD card is not there, don't do anything more:
-  if (digitalRead(cardDetect) == LOW) {
-    digitalWrite(errorLed, HIGH);
-    return;
-  }
-  // turn off the error LED:
-  digitalWrite(errorLed, LOW);
-
-  if (millis()  - lastWriteTime >=  interval * 100)
-    Serial.println(dataFieldNames);
-
-  // read sensors every 10 seconds
-  if (millis()  - lastWriteTime >=  interval) {
+  // read sensors and write the log file every 'interval' milliseconds
+  if (!lastReadTime || millis() - lastReadTime >= readInterval) {
     char logLine[256];
+    digitalWrite(writeLed, HIGH);
     int offset = snprintf(logLine, sizeof(logLine), "%d", millis());
     offset = appendCommaFloat(logLine, sizeof(logLine), offset, batteryVoltage());
     offset = appendCommaFloat(logLine, sizeof(logLine), offset, bme.readTemperature());
     offset = appendCommaFloat(logLine, sizeof(logLine), offset, bme.readHumidity());
     offset = appendCommaFloat(logLine, sizeof(logLine), offset, bme.readPressure() / 100.0F);
-
+    if (!lastReadTime || millis() - lastReadTime >= logInterval)
+      Serial.println(dataFieldNames);
     Serial.println(logLine);
+    delay(5);
+    digitalWrite(writeLed, LOW);
+    lastReadTime = millis();
 
-    File logFile = SD.open(fileName, FILE_WRITE);   // open the log file
-    if (logFile) {                                  // if you can write to the log file,
-      digitalWrite(writeLed, HIGH);                 // turn on the write LED
-      logFile.println(logLine);                       // write a line of data
-      logFile.close();                              // close the file
-      lastWriteTime = millis();
+    bool sdWasInited = sdInited;
+    if (!sdInited) 
+      sdInited = startSDCard();
+
+    if (digitalRead(cardDetect) == LOW) {
+      if (sdWasInited) {
+        Serial.println("SD card removed");
+        blinkError(2);
+      }
+      sdInited = false;
+      SD.end();
     }
-    delay(100);
-    digitalWrite(writeLed, LOW);                    // turn off the write LED
+
+    if (sdInited && (!sdWasInited || !lastWriteTime || millis() - lastWriteTime >= logInterval)) {
+      File logFile = SD.open(fileName, FILE_WRITE);   // open the log file
+      if (logFile) {                                  // if you can write to the log file,
+        digitalWrite(writeLed, HIGH);                 // turn on the write LED
+        if (!sdWasInited) {
+          logFile.println(dataFieldNames);
+          Serial.println("SD card found, log file started");
+        }
+        logFile.println(logLine);                     // write a line of data
+        logFile.close();                              // close the file
+        lastWriteTime = millis();
+      }
+      delay(500);
+      digitalWrite(writeLed, LOW);                    // turn off the write LED
+      Serial.println(dataFieldNames);
+      delay((millis() + readInterval) % readInterval);
+    }
   }
 }
 
